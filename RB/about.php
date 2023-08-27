417fdc98c2c7af74e977570fda6027d88cb1644d
Asif6619 <141047470+Asif6619@users.noreply.github.com>
2023-08-27 23:04:50 +0600

    Add files via upload

---
+++ RB/about.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume Builder</title>
+    <link rel="stylesheet" href="aboutstyle.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index_right.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/8951f54fb0.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+<div class="container">
+<button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div id="main_div">
+        <ul>
+            <li><a href="#!"><button type="button" class="btn btn-primary"><span style="color:Orange;font-weight: bold;">Cv</span><span style="color: #e3d723;font-weight: bold;">Generator</span></button></a></li>
+            <li><a href="#home"><button type="button" class="btn btn-primary">Home</button></a></li>
+            <li><a href="src/template/index.php"><button type="button" class="btn btn-primary">Create CV</button></a></li>
+            <li><a href="#contact"><button type="button" class="btn btn-primary">About</button></a></li>
+            <li><a href="#contact"><button type="button" class="btn btn-primary">Contact</button></a></li>
+               
+            <li style="float:right"><a class="active" href="src/cv-template"><button type="button" class="btn btn-primary">Sign Up</button></a></li>
+            <li style="float:right"><a class="active" href="src/cv-template"><button type="button" class="btn btn-primary"><i class="fa-regular fa-user"></i>Log In</button></a></li>
+            <li style="float:right"><a class="active" href="https://twitter.com/i/flow/login"><button type="button" class="btn btn-primary"><i class="fab fa-twitter"></i></button></a></li>
+            <li style="float:right"><a class="active" href="https://www.instagram.com/"><button type="button" class="btn btn-primary"><i class="fab fa-instagram"></i></button></a></li>
+            <li style="float:right"><a class="active" href="src/Login and Register/register.php"><button type="button" class="btn btn-primary"><i class="fas fa-shopping-cart"></i></button></a></li>
+          </ul>
+</nav>
+    
+<section>
+        <!-- Intro -->
+        <div id="intro" class="bg-image vh-100" style="
+              background-image: url('hero.jpg');
+            ">
+            <div style="display: flex; text-align: center; margin-left: 140px; padding-top: 5%;">
+                <div class="left">
+                    <h3 style="font-size: 60px;margin-bottom: 30px;">
+                    Let your skills shine with a well-crafted resume <br> with <span style="color: #1a70f0;font-weight: bold;">Cv</span><span style="color: #e3d723;font-weight: bold;">Generator</span>
+                    </h3>
+                    <a href="src/Login and Register/index.php"><button type="button" class="btn btn-warning btn-lg">Create CV</button></a>
+                    <a href="src/Login and Register/index.php"><button type="button" class="btn btn-primary <a href="#" class="btn btn-primary active" role="button">Update your CV</button></a>
+                    </a>
+                </div>
+                <div class="right">
+                    <div class="container">
+                        <div class="box">
+                            <div class="imgBx">
+                                <img src="src/temImg/1.jpg">
+                            </div>
+                            <div class="content">
+                                <div>
+                                    <h2>Templete 1</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                                    </p>
+                                    <a href="src/Login and Register/index.php"><button type="button" class="btn btn-success">Try It</button></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="imgBx">
+                                <img src="src/temImg/2.jpeg">
+                            </div>
+                            <div class="content">
+                                <div>
+                                    <h2>Template2</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                                    </p>
+                                    <a href="src/Login and Register/index.php"><button type="button" class="btn btn-success">Try It</button></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="imgBx">
+                                <img src="src/temImg/3.jpg">
+                            </div>
+                            <div class="content">
+                                <div>
+                                    <h2>Template 3</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                                    </p>
+                                    <a href="src/Login and Register/index.php"><button type="button" class="btn btn-success">Try It</button></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="imgBx">
+                                <img src="src/temImg/4.png">
+                            </div>
+                            <div class="content">
+                                <div>
+                                    <h2>Template 4</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                                    </p>
+                                    <a href="src/Login and Register/index.php"><button type="button" class="btn btn-success">Try It</button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Intro -->
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    
+</body>
+</html>
